e11623625e3a4c5579d914273a87327c61c53095
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-03 17:24:52 +0100

    refactor: throw exception in old implementation

--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -84,27 +84,7 @@ public class Item
         }
 
         if (Name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (Quality < 50) {
-                Quality = Quality + 1;
-
-                if (SellIn < 11) {
-                    if (Quality < 50) {
-                        Quality = Quality + 1;
-                    }
-                }
-
-                if (SellIn < 6) {
-                    if (Quality < 50) {
-                        Quality = Quality + 1;
-                    }
-                }
-            }
-
-            SellIn = SellIn - 1;
-
-            if (SellIn < 0) {
-                Quality = Quality - Quality;
-            }
+            throw new NotImplementedException("Use subclass instead");
         }
         else {
             if (Name == "Sulfuras, Hand of Ragnaros") {
