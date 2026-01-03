bd580a812cf03b0598d42dbbe1758da28ca183d0
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-03 17:24:21 +0100

    refactor: override update quality in backstage

--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -44,7 +44,30 @@ public class Item
 
     class BackstagePass : Item {
         public BackstagePass(string name, int sellIn, int quality) : base(name, sellIn, quality) { }
-        
+
+        public override void UpdateQuality() {
+            if (Quality < 50) {
+                Quality = Quality + 1;
+
+                if (SellIn < 11) {
+                    if (Quality < 50) {
+                        Quality = Quality + 1;
+                    }
+                }
+
+                if (SellIn < 6) {
+                    if (Quality < 50) {
+                        Quality = Quality + 1;
+                    }
+                }
+            }
+
+            SellIn = SellIn - 1;
+
+            if (SellIn < 0) {
+                Quality = Quality - Quality;
+            }
+        }
     }
 
     // Long method
