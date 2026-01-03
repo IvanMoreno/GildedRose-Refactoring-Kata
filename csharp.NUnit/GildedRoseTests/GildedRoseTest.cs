9b5085e065174a289aa8b0145cabadf27af4cef4
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-03 17:17:42 +0100

    refactor: convert object initializer into constructor

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -9,7 +9,7 @@ public class GildedRoseTest
     [Test, Ignore("Using this kata only to practice smell identification and refactoring, so assuming the risk of not having unit test coverage")]
     public void Foo()
     {
-        var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+        var items = new List<Item> { new Item(name: "foo", sellIn: 0, quality: 0) };
         var app = new GildedRose(items);
         app.UpdateQuality();
         Assert.That(items[0].Name, Is.EqualTo("fixme"));
