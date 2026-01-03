5cfabd192cf309ec9506c0b0095fbd5631c3b15c
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-03 17:05:19 +0100

    refactor: inline methods

--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -28,55 +28,94 @@ public class Item
         }
         else {
             if (Name == "Backstage passes to a TAFKAL80ETC concert") {
-                UpdateBackstageQuality(true);
-            }
-            else {
-                UpdateBackstageQuality(false);
-            }
-        }
-    }
+                if (true) {
+                    if (Quality < 50) {
+                        Quality = Quality + 1;
 
-    void UpdateBackstageQuality(bool isBackstagePass) {
-        if (isBackstagePass) {
-            if (Quality < 50) {
-                Quality = Quality + 1;
+                        if (true) {
+                            if (SellIn < 11) {
+                                if (Quality < 50) {
+                                    Quality = Quality + 1;
+                                }
+                            }
 
-                if (isBackstagePass) {
-                    if (SellIn < 11) {
-                        if (Quality < 50) {
-                            Quality = Quality + 1;
+                            if (SellIn < 6) {
+                                if (Quality < 50) {
+                                    Quality = Quality + 1;
+                                }
+                            }
                         }
                     }
+                }
+                else {
+                    if (Quality > 0) {
+                        if (Name != "Sulfuras, Hand of Ragnaros") {
+                            Quality = Quality - 1;
+                        }
+                    }
+                }
 
-                    if (SellIn < 6) {
-                        if (Quality < 50) {
-                            Quality = Quality + 1;
+                if (Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    SellIn = SellIn - 1;
+                }
+
+                if (SellIn < 0) {
+                    if (true) {
+                        Quality = Quality - Quality;
+                    }
+                    else {
+                        if (Quality > 0) {
+                            if (Name != "Sulfuras, Hand of Ragnaros") {
+                                Quality = Quality - 1;
+                            }
                         }
                     }
                 }
             }
-        }
-        else {
-            if (Quality > 0) {
-                if (Name != "Sulfuras, Hand of Ragnaros") {
-                    Quality = Quality - 1;
-                }
-            }
-        }
-
-        if (Name != "Sulfuras, Hand of Ragnaros")
-        {
-            SellIn = SellIn - 1;
-        }
-
-        if (SellIn < 0) {
-            if (isBackstagePass) {
-                Quality = Quality - Quality;
-            }
             else {
-                if (Quality > 0) {
-                    if (Name != "Sulfuras, Hand of Ragnaros") {
-                        Quality = Quality - 1;
+                if (false) {
+                    if (Quality < 50) {
+                        Quality = Quality + 1;
+
+                        if (false) {
+                            if (SellIn < 11) {
+                                if (Quality < 50) {
+                                    Quality = Quality + 1;
+                                }
+                            }
+
+                            if (SellIn < 6) {
+                                if (Quality < 50) {
+                                    Quality = Quality + 1;
+                                }
+                            }
+                        }
+                    }
+                }
+                else {
+                    if (Quality > 0) {
+                        if (Name != "Sulfuras, Hand of Ragnaros") {
+                            Quality = Quality - 1;
+                        }
+                    }
+                }
+
+                if (Name != "Sulfuras, Hand of Ragnaros")
+                {
+                    SellIn = SellIn - 1;
+                }
+
+                if (SellIn < 0) {
+                    if (false) {
+                        Quality = Quality - Quality;
+                    }
+                    else {
+                        if (Quality > 0) {
+                            if (Name != "Sulfuras, Hand of Ragnaros") {
+                                Quality = Quality - 1;
+                            }
+                        }
                     }
                 }
             }
