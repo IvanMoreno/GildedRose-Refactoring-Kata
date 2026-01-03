077e3320cfc4bc5d697a53c09e8321cb7fb83b7e
IvanMoreno <ivanmorenogarciafresneda@gmail.com>
2026-01-03 17:22:07 +0100

    refactor: remove redundant else

--- csharp.NUnit/GildedRose/Item.cs
+++ csharp.NUnit/GildedRose/Item.cs
@@ -50,45 +50,44 @@ public class Item
         if (Name == "Aged Brie") {
             throw new NotImplementedException("Use subclass instead");
         }
+
+        if (Name == "Backstage passes to a TAFKAL80ETC concert") {
+            if (Quality < 50) {
+                Quality = Quality + 1;
+
+                if (SellIn < 11) {
+                    if (Quality < 50) {
+                        Quality = Quality + 1;
+                    }
+                }
+
+                if (SellIn < 6) {
+                    if (Quality < 50) {
+                        Quality = Quality + 1;
+                    }
+                }
+            }
+
+            SellIn = SellIn - 1;
+
+            if (SellIn < 0) {
+                Quality = Quality - Quality;
+            }
+        }
         else {
-            if (Name == "Backstage passes to a TAFKAL80ETC concert") {
-                if (Quality < 50) {
-                    Quality = Quality + 1;
-
-                    if (SellIn < 11) {
-                        if (Quality < 50) {
-                            Quality = Quality + 1;
-                        }
-                    }
-
-                    if (SellIn < 6) {
-                        if (Quality < 50) {
-                            Quality = Quality + 1;
-                        }
-                    }
+            if (Name == "Sulfuras, Hand of Ragnaros") {
+                // Do Nothing
+            }
+            else {
+                if (Quality > 0) {
+                    Quality = Quality - 1;
                 }
 
                 SellIn = SellIn - 1;
 
                 if (SellIn < 0) {
-                    Quality = Quality - Quality;
-                }
-            }
-            else {
-                if (Name == "Sulfuras, Hand of Ragnaros") {
-                    // Do Nothing
-                }
-                else {
                     if (Quality > 0) {
                         Quality = Quality - 1;
-                    }
-
-                    SellIn = SellIn - 1;
-
-                    if (SellIn < 0) {
-                        if (Quality > 0) {
-                            Quality = Quality - 1;
-                        }
                     }
                 }
             }
